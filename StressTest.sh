f46f095d09c5fba6a4560177d06b6ea814c3e3e5
root <root@lab-scripts.(none)>
2014-01-22 19:09:45 -0200

    Changes 22-01-2014

--- StressTest.sh
+++ StressTest.sh
@@ -18,3 +18,34 @@
 #
 #### Inicio do Script ####
 
+# Timestamp
+TIMESTAMP=$(date +"%Y%m%d%H%M")
+
+# Numero de usuarios concorrentes
+CONCURRENT=10
+
+# Numero de repeticoes com base no numero de usuarios (REPETION * CONCURRENT)
+REPETION=1
+
+# Intervalo em segundos de cada acesso
+DELAY=10
+
+# Tempo de duracao do teste (1M = 1 minutos, 1H = 1 hora, 60s = 60 segundos)
+TIME=1M
+
+# Arquivo de log do teste
+LOG=logs/logs_$TIMESTAMP.log
+
+# Relatorio HTML
+HTML=report/report_$TIMESTAMP.html
+
+# URL do da aplicacao a ser testada
+URL=http://superlutas-tmp.terra.com.br
+
+touch $LOG
+siege -c$CONCURRENT -d$DELAY -r$REPETION -l$LOG -v $URL
+
+cp report.html $HTML
+
+# Criacao do Relatorio
+sed -i "s/URL/$URL/g" $HTML
